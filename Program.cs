d11e8a3e25f18ce4789b6ae2ad027b1f432da813
Benjamín Peralta <benjaminp158@gmail.com>
2025-07-24 12:24:09 -0300

    Removed innecesary using

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 
 IConfigurationRoot config = new ConfigurationBuilder()
     .AddJsonFile("./appsettings.json")
